6120ec149ae9efeeddd71a676f595269e27e13aa
AgustinaNunez98 <agusanunez@gmail.com>
2026-07-13 17:27:15 -0300

    Corrige carga de destinos desde API

--- js/script.js
+++ js/script.js
@@ -166,7 +166,7 @@ mensajeError.textContent = "Por favor, ingrese un correo electrónico válido.";
 function cargarDestinos() {
   if (!contenedorDestinos) return;
 
-  const destinos = [
+  constDestinos = [
     "Cataratas del Iguazú",
     "Glaciar Perito Moreno",
     "San Carlos de Bariloche"
